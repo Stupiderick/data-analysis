39935d163a16bf5233652d88eb28cc7fe24e4279
Erick Li <ericklek1994@gmail.com>
2017-09-30 18:12:15 -0500

    Update airports.py

--- airport_code_name/airports.py
+++ airport_code_name/airports.py
@@ -4,12 +4,15 @@
 def convert(theFile):
     '''
     Input the file and then convert the raw data to readable sentences.
+    The raw data are written as:
+        1st col: IATA code
+        2nd col: city name
     @param: the path of the file
     '''
-    with open(theFile, 'r') as f:
-        for line in f:
+    with open(theFile, 'r') as myFile:
+        for line in myFile:
             item = line.split('\n')
             code = item[1][:3]
             city = item[1][4:]
-            print('The code of the airport of {0} is {1}'.format(city, code))
+            print('The code of the airport of {0} is {1}'.format(city, code)). # Print out the readable sentences.
             
